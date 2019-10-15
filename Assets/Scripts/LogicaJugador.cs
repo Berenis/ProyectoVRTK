f00b55b9fa9ec8f9a95152cd9bb9a91753ad9462
Berenis <54555823+Berenis@users.noreply.github.com>
2019-10-14 21:18:24 -0300

    primer commit

---
+++ Assets/Scripts/LogicaJugador.cs
@@ -0,0 +1,39 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogicaJugador : MonoBehaviour
+{
+    public Vida vida;
+    public bool Vida0 = false;
+    public Puntaje puntaje;
+    [SerializeField] private Animator animadorPerder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vida = GetComponent<Vida>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RevisarVida();
+    }
+
+    void RevisarVida()
+    {
+        if (Vida0) return;
+        if(vida.valor <= 0)
+        {
+            Vida0 = true;
+            Invoke("ReiniciarJuego", 2f);
+        }
+    }
+
+    void ReiniciarJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        puntaje.valor = 0;
+    }
+}
